915891487dc70257daf066063971269a2c2a7f11
caijingquan <1990frog@gmail.com>
2024-01-15 15:46:50 +0800

    first commit

---
+++ lua/config/options.lua
@@ -0,0 +1,8 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
+local opt = vim.opt
+
+opt.spelllang = { "en", "cjk" }
+-- opt.spelllang = { "utf-8" }
